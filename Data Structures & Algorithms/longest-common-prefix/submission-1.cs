cc4cdf9a8b2bc877c1511b3c3b775d826625b682
Cuong <178352543+Curian74@users.noreply.github.com>
2026-04-23 18:19:42 +0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        int min = strs[0].Length;
+        foreach (string s in strs) {
+            if (s.Length < min)
+            min = s.Length;
+        }
+        int i = 0;
+        for (; i < min; i++) {
+            Console.WriteLine("a");
+            foreach (string s in strs) {
+                if (s[i] != strs[0][i]){
+                    return strs[0].Substring(0, i);
+                }
+            }
+        }
+        return strs[0].Substring(0, i);
+    }
+}
