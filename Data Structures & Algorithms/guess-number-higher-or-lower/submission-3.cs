a14745be385be046c63a38d0ad91500c4cc647f7
Cuong <178352543+Curian74@users.noreply.github.com>
2026-04-27 18:50:08 +0700

    Add: guess-number-higher-or-lower - submission-3

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-3.cs
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        int l = 1, r = n;
+        while (l <= n) {
+            int m = l + (r - l) / 2;
+            if (guess(m) == 0) return m;
+            if (guess(m) == 1) {
+                l = m + 1;
+            }
+            else if (guess(m) == -1) {
+                r = m;
+            }
+        }
+        return l;
+    }
+}
