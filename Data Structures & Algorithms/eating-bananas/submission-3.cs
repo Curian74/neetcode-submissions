b7427f1736ddb3f6ddb4033b4adf80abe66ed002
Cuong <178352543+Curian74@users.noreply.github.com>
2026-05-03 10:52:54 +0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        int res = int.MaxValue;
+        for (int i = 1; ; i++) {
+            long hSpent = 0;
+            foreach (int j in piles) {
+                if (hSpent > h) break;
+                if (j < i) hSpent += 1;
+                else {
+                    long d = (j + i - 1) / i;
+                    hSpent += (long)d;
+                }
+            }
+            if (hSpent <= h) {
+                res = Math.Min(res, i);
+                break;
+            }
+        }
+
+        return res;
+    }
+}
