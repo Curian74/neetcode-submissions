3e5c35332673ff1334f6bc75113ec695f7179cb1
Cuong <178352543+Curian74@users.noreply.github.com>
2026-05-14 20:22:16 +0700

    Add: remove-linked-list-elements - submission-0

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-0.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        var cur = head;
+        var dummy = new ListNode();
+        var res = dummy;
+        while (cur != null) {
+            if (cur.val != val) {
+                var temp = new ListNode(cur.val);
+                dummy.next = temp;
+                dummy = dummy.next;
+            }
+            cur = cur.next;
+        }
+        return res.next;
+    }
+}
